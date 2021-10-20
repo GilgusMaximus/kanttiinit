1cd56bf71d2c75e83a4c14c237b509975cf4a326
lepasq <pasquarelli.le@gmail.com>
2021-10-20 12:25:32 +0300

    Add example meals

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# kanttiinit
+# Kanttiinit
+
+
+## Routes
+
+`/restaurants`: displays information for all restaurants  
+`/restaurants/:name`: displays information for specific restaurant  
+`/restaurants/:name/meals`: displays all meals of the day for specific restaurant  
+`/restaurants/:name/ratings`: displays restaurant ratings for a specific restaurant  
--- routes/meals.js
+++ routes/meals.js
@@ -1,9 +1,49 @@
 var express = require("express");
 var router = express.Router();
 
+
+let meals = [
+    {
+        id: 0,
+        name: "Kebab Casserole",
+        type: "Lunch 1",
+        diets: [
+            "Vegan", "Lactose-free", "Gluten-free"
+        ],
+        image: "https://assets.bonappetit.com/photos/5d977e513b30f40008a4785e/1:1/w_1968,h_1968,c_limit/Basically-Hammy-Chickpea-Soup.jpg"
+    },
+    {
+        id: 1,
+        name: "Whole Grain Rice",
+        type: "Side",
+        diets: [
+            "Vegan", "Lactose-free", "Gluten-free"
+        ],
+        image: "https://assets.bonappetit.com/photos/5d977e513b30f40008a4785e/1:1/w_1968,h_1968,c_limit/Basically-Hammy-Chickpea-Soup.jpg"
+    },
+    {
+        id: 2,
+        name: "Chicken with Curry",
+        type: "Lunch 2",
+        diets: [
+            "Meat", "Lactose",
+        ],
+        image: "https://assets.bonappetit.com/photos/5d977e513b30f40008a4785e/1:1/w_1968,h_1968,c_limit/Basically-Hammy-Chickpea-Soup.jpg"
+    },
+    {
+        id: 3,
+        name: "Chickpea Soup",
+        type: "Lunch 3",
+        diets: [
+            "Vegan", "Lactose-free"
+        ],
+        image: "https://assets.bonappetit.com/photos/5d977e513b30f40008a4785e/1:1/w_1968,h_1968,c_limit/Basically-Hammy-Chickpea-Soup.jpg"
+    },
+]
+
 /* GET all restaurants */
 router.get("/", function (req, res, next) {
-    res.send("HELLO " + req.restaurant);
+    res.send(meals);
 });
 
 module.exports = router;
