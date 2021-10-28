1b6553d1656fcbed5322643d0c298da306686380
lepasq <pasquarelli.le@gmail.com>
2021-10-28 09:49:46 +0300

    Remove unnecessary import

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 var logger = require('morgan');
 var sassMiddleware = require('node-sass-middleware');
 var cors = require('cors')
