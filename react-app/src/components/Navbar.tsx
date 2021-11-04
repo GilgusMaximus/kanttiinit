fe575a381b4aeca93ea03c6ae02cb1b482aa50a0
lepasq <pasquarelli.le@gmail.com>
2021-11-04 13:02:53 +0200

    Add dates to navbar

--- react-app/src/components/Navbar.tsx
+++ react-app/src/components/Navbar.tsx
@@ -9,27 +9,35 @@ import MenuIcon from '@mui/icons-material/Menu';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 
 export default function ButtonAppBar() {
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Kanttiinit
-          </Typography>
-          <Button color="inherit">
-            <AccountCircle />
-        </Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
+
+    let currentDates = []
+    for (let i = 0; i < 7; i++) {
+        currentDates.push(new Date(Date.now() + i * 24 * 60 * 60 * 1000))
+    }
+
+    return (
+        <Box sx={{flexGrow: 1}}>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{mr: 2}}
+                    >
+                        <MenuIcon/>
+                    </IconButton>
+                    {currentDates.map((x) => (
+                        <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
+                            {(x.toLocaleDateString("en-EN", {weekday: 'long'}).substr(0, 2).toUpperCase()) + " " +  ([x.getUTCDate(), x.getUTCMonth()+1].join('-'))}
+                        </Typography>
+                    ))}
+                    <Button color="inherit">
+                        <AccountCircle/>
+                    </Button>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
 }
