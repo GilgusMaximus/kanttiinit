444b2dfcbf94f0271811c579c3ceef016cfa2240
Luca Hohmann <hohmann.luca@gmx.de>
2021-11-04 17:54:52 +0200

    Custom Fonts
    Added:
    - Custom Font - The Open Sans font is now loaded into the page via a custom theme. The theme can also be used for further styling

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,18 +1,29 @@
 import React, {Component} from 'react';
 import './App.css';
+import './components/root.css';
 import Navbar from './components/Navbar'
 import Restaurants from './components/Restaurants'
 import RightDrawer from "./components/RightDrawer";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+    typography: {
+      fontFamily: [
+        'Open Sans'
+      ].join(','),
+   },});
 
 class App extends Component {
 
     render() {
         return (
+            <ThemeProvider theme={theme}>
             <div className="App">
                 <Navbar/>
                 {/* <RightDrawer meals={this.state.res}/> */}
                 <Restaurants/>
             </div>
+            </ThemeProvider>
         );
     }
 }
