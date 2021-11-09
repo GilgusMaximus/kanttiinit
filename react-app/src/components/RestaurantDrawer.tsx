04d210546d8651658f0fca08ebb2eb6a3a5f2a4e
DuBento <duarte.bento@tecnico.ulisboa.pt>
2021-11-09 14:38:53 +0200

    Fixed zoom bug for DrawerHeader

--- react-app/src/components/RestaurantDrawer.tsx
+++ react-app/src/components/RestaurantDrawer.tsx
@@ -13,9 +13,6 @@ const DrawerHeader = styled("div")(({ theme }) => ({
     display: "flex",
     alignItems: "center",
     padding: theme.spacing(0, 1),
-    // necessary for content to be below app bar
-    ...theme.mixins.toolbar,
-    justifyContent: "flex-start"
 }));
 
 class RestaurantDrawer extends React.Component<{
