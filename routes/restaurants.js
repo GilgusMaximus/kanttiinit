0ec97b34a12ec7ea591ddb59664d4153081a3c53
lepasq <pasquarelli.le@gmail.com>
2021-11-16 22:54:56 +0200

    Implement categories as arrays

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -10,6 +10,24 @@ const db = require('../db/datastore');
 /* GET all restaurants and res meals */
 router.get("/", function (req, res, next) {
     db.getAllRestaurantsAndMeals(req.query["day"]).then(response => {
+        for (let j = 0; j < response.length; j++) {
+            restaurant = response[j]
+            categories = {}
+            if (!restaurant.meals) {
+                console.log("continuing")
+                continue;
+            }
+            for (let i = 0; i < restaurant.meals.length || 0; i++) {
+                let meal = restaurant.meals[i]
+                if (meal.category in categories) {
+                    categories[meal.category].push(meal)
+                } else {
+                    categories[meal.category] = [meal]
+                }
+            }
+            console.log(categories)
+            restaurant.meals = categories
+        }
         res.send(response)
     });
 });
