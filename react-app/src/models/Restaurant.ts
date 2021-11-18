21b08b372c71b794108533474b2b857b17438a86
lepasq <pasquarelli.le@gmail.com>
2021-11-18 20:31:14 +0200

    Update restaurant model

--- react-app/src/models/Restaurant.ts
+++ react-app/src/models/Restaurant.ts
@@ -1,6 +1,6 @@
-import { Meal } from './Meal';
+import {Meal} from './Meal';
 
-type Coordinates = {latitude: number, longitude: number};
+type Coordinates = { latitude: number, longitude: number };
 
 export class Restaurant {
     id: string | null = null;
@@ -8,24 +8,27 @@ export class Restaurant {
     location: Coordinates = {latitude: 0, longitude: 0};
     pricing: number | null = null;
     url: string | null = null;
-    meals: Meal[] = [];
-  
-    constructor(jsonObj?: Restaurant) {
-      if (jsonObj) {
-        this.id = jsonObj.id;
-        this.name = jsonObj.name;
-        if (jsonObj.location!) {
-          this.location.latitude = jsonObj.location.latitude;
-          this.location.longitude = jsonObj.location.longitude;
-        }
-        this.pricing = jsonObj.pricing;
-        this.url = jsonObj.url;
+    meals: Map<String, Meal[]> = new Map<String, []>()
+    mealList: Meal[] = [];
 
-        if (jsonObj.meals)
-          this.meals = jsonObj.meals.map(
-            (meal: Meal) => new Meal(meal)
-          );
-      }
+    constructor(jsonObj?: Restaurant) {
+        if (jsonObj) {
+            this.id = jsonObj.id;
+            this.name = jsonObj.name;
+            if (jsonObj.location!) {
+                this.location.latitude = jsonObj.location.latitude;
+                this.location.longitude = jsonObj.location.longitude;
+            }
+            this.pricing = jsonObj.pricing;
+            this.url = jsonObj.url;
+
+            if (jsonObj.meals) {
+                console.log(jsonObj.meals)
+                jsonObj.meals.forEach((meals, name) => {
+                    this.meals.set(name, meals)
+                    meals.forEach(m => this.mealList.push(m))
+                })
+            }
+        }
     }
-  }
-  
+}
