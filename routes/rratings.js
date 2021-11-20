dc042657a921d0da02a64d30ce32fd977916bd5d
lepasq <pasquarelli.le@gmail.com>
2021-11-20 21:33:03 +0200

    Add ratings-backend (without checking duplicates

--- routes/rratings.js
+++ routes/rratings.js
@@ -5,9 +5,9 @@ let db = require('../db/datastore')
 
 /* GET all restaurants */
 router.get("/", function (req, res, next) {
-    db.getRestaurantRatings(req.restaurant).then(response => {
-        console.log(response)
-        res.send(response)
+    db.getRestaurantRating(req.restaurant).then(response => {
+        console.log("AVG RATING: ", response, "\n")
+        res.send({"rating": response})
     })
 });
 
@@ -15,10 +15,13 @@ router.get("/", function (req, res, next) {
 router.post('/', (req, res, next) => {
     const rest = req.restaurant
     const rating = req.body.rating;
+    const name = "admin"
 
-    db.addRestaurantRating("admin", rest, rating).then(r => {
+    db.addRestaurantRating(name, rest, rating).then(r => {
         if (r === -1) {
-            return res.sendStatus(400)
+            return res.status(400).send({error: "Your rating must be in between 1 and 5."})
+        } else if (r === -2) {
+            return res.status(400).send({error: "You already added a rating."})
         }
         return res.sendStatus(200)
     })
